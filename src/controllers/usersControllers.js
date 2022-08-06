2152354262038b71660009c098b66a5c206c9778
Nayara <nrdeandrade@gmail.com>
2022-08-06 18:33:21 -0300

    refactor: add userRepositoriy to usersControllers

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,30 +1,18 @@
-import connection from '../database/database.js';
+import userRepository from '../repositories/userRepository.js';
 
 export default async function getUserUrls(req, res) {
     const { id } = req.user;
-    console.log(req.user)
 
     try {
-        const { rows: validUser } = await connection.query(`
-        SELECT users
-        WHERE id = $1
-        `,[id]);
+        const { rows: validUser } = await userRepository.getUserById(id);
+
         if(validUser.length < 1) {
             return res.status(404).send('User not found');
-        }    
+        }
+        
+        const { rows: userData } = await userRepository.getUserData(id);
 
-        const { rows: userData } = await connection.query(`
-        SELECT users.id, users.name, SUM(links.clicks) as "visitCount"
-        FROM users
-        JOIN links ON links."userId" = users.id
-        WHERE users.id = $1
-        GROUP BY users.id
-        `, [id]);
-
-        const { rows: userUrls } = await connection.query(`
-        SELECT id, "shortUrl", url, clicks AS "visitCount" 
-        FROM links 
-        WHERE "userId" = $1`, [id]);
+        const { rows: userUrls } = await userRepository.getUserUrls(id);
         
         res.status(200).send({ ...userData[0], shortenedUrls: userUrls });
     } catch (error) {
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,21 +1,50 @@
 import connection from '../database/database.js';
 
-export async function getUserByEmail(email) {
+async function getUserByEmail(email) {
     return await connection.query(`
     SELECT * FROM users 
     WHERE email = $1`, [email]);
 }
 
-export async function insertUser(name, email, encryptKey) {
+async function insertUser(name, email, encryptKey) {
     return await connection.query(`
     INSERT INTO users (name, email, password) 
     VALUES ($1, $2, $3)
     `, [name, email, encryptKey]);
 }
 
+async function getUserById(id) {
+    return await connection.query(`
+    SELECT * 
+    FROM users
+    WHERE id = $1
+    `,[id]);
+}
+
+async function getUserData(id) {
+    return await connection.query(`
+    SELECT users.id, users.name, SUM(links.clicks) as "visitCount"
+    FROM users
+    JOIN links ON links."userId" = users.id
+    WHERE users.id = $1
+    GROUP BY users.id
+    `, [id]);
+}
+
+async function getUserUrls(id) {
+    return await connection.query(`
+    SELECT id, "shortUrl", url, clicks AS "visitCount" 
+    FROM links 
+    WHERE "userId" = $1
+    `, [id]);
+}
+
 const userRepository = {
     getUserByEmail,
-    insertUser
+    insertUser,
+    getUserById,
+    getUserData,
+    getUserUrls
 }
 
 export default userRepository;
