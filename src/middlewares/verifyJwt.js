cafb2991ed48a0b62db8f60cdc644173f5162dda
Nayara <nrdeandrade@gmail.com>
2022-08-06 19:23:40 -0300

    style: remove consoles

--- src/middlewares/verifyJwt.js
+++ src/middlewares/verifyJwt.js
@@ -4,7 +4,6 @@ export default function verifyJwt(req, res, next) {
     const SECRET_KEY = process.env.JWT_SECRET;
     const { authorization } = req.headers
     const token = authorization?.replace('Bearer', '').trim();
-    console.log(req.headers, token)
 
     if(!token) {
         return res.status(401).send('Access denied. No token provided.');
